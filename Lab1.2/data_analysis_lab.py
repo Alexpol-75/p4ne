7c0bc93cc63bd765888b2e675d769af0060ac363
Alexpol-75 <elfalexpol@gmail.com>
2022-05-24 18:09:33 +0300

    Lab1.2

--- Lab1.2/data_analysis_lab.py
+++ Lab1.2/data_analysis_lab.py
@@ -3,10 +3,11 @@ from matplotlib import pyplot
 
 def getv(x): return x.value
 
-# Извлечь столбцы A, C, D
+# Открыть книгу Excel
 wb = load_workbook('data_analysis_lab.xlsx')
 sheet = wb['Data']
 
+# Извлечь столбцы A, C, D
 list_x = list(map(getv, sheet['A'][1:]))
 list_c = list(map(getv, sheet['C'][1:]))
 list_d = list(map(getv, sheet['D'][1:]))
