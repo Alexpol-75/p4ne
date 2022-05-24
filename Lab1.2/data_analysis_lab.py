8506e56199244a846dbe8a8b1a84571532703681
Alexpol-75 <elfalexpol@gmail.com>
2022-05-24 18:05:18 +0300

    Lab1.2

---
+++ Lab1.2/data_analysis_lab.py
@@ -0,0 +1,16 @@
+from openpyxl import load_workbook
+from matplotlib import pyplot
+
+def getv(x): return x.value
+
+# Извлечь столбцы A, C, D
+wb = load_workbook('data_analysis_lab.xlsx')
+sheet = wb['Data']
+
+list_x = list(map(getv, sheet['A'][1:]))
+list_c = list(map(getv, sheet['C'][1:]))
+list_d = list(map(getv, sheet['D'][1:]))
+
+pyplot.plot(list_x, list_c, label='Temp')
+pyplot.plot(list_x, list_d, label='Act')
+pyplot.show()
