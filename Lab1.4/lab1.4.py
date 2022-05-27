ceff48201656b80822fcd85782ecb39f574e022d
Alexpol-75 <elfalexpol@gmail.com>
2022-05-27 18:43:46 +0300

    Lab1.4

--- Lab1.4/lab1.4.py
+++ Lab1.4/lab1.4.py
@@ -6,11 +6,21 @@ class IPv4RandomNetwork(ipaddress.IPv4Network):
         ipaddress.IPv4Network.__init__(self, (random.randint(0x0b000000,0xdf000000), random.randint(8,24)), strict=False)
         self.regular = not self.is_private
 
-L = []
+def keyfunc(net):
+    return int(net.netmask) * 2**32 + int(net.network_address)
 
+# Генерируем список сетей
+L = []
 while len(L) < 20:
     net = IPv4RandomNetwork()
     if net.regular: L.append(net)
 
+# Печатаем список в оригинальном порядке
 #for i in L: print(i, i.regular)
+print('\n***** Unsorted:')
+for i in range(len(L)): print("#", i+1, "network: ", L[i], "regular:", L[i].regular)
+
+# Сортируем и печатаем список снова
+L.sort(key=keyfunc)
+print('\n***** Sorted:')
 for i in range(len(L)): print("#", i+1, "network: ", L[i], "regular:", L[i].regular)
