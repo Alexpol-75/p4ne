7317840748d801d43540fd429f37f3bf7273c35d
Alexpol-75 <elfalexpol@gmail.com>
2022-05-30 16:45:12 +0300

    Lab1.5

--- Lab1.5 files/config_pars.py
+++ Lab1.5 files/config_pars.py
@@ -2,14 +2,14 @@ import glob
 
 L_ip = []   # Список ip-адресов
 
-for f in glob.glob('c:\\Python\\config_files\\*.txt'):
-    with open(f) as textf:
-        strings = list(textf)
-        for line in strings:
-            pos = line.find('ip address ')
-            if pos >0 and 'prefix' not in line:
-                ip_mask =  line[pos:-1].replace('ip address ','').strip()
-                L_ip.append(ip_mask)
+for f in glob.glob('c:\\Python\\config_files\\*.txt'):      # Список файлов *.txt
+    with open(f) as text_file:
+        strings = list(text_file)                           # Читаем текстовый файл из списка
+        for line in strings:                                # Цикл по строкам
+            pos = line.find('ip address ')                  # Ищем признак ip-адреса и
+            if pos > 0 and 'prefix' not in line:
+                ip_mask =  line[pos:-1].replace('ip address ','').strip()   # Оставляем нужное
+                L_ip.append(ip_mask)                                        # Добавляем адрес в список
 
 L_unique_ip = list(set(L_ip))       # Список уникальных ip-адресов
 for ip in L_unique_ip: print(ip)    # Печать списка
